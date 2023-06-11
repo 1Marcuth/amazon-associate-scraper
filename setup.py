f33448fddb99f3ea406f97ccc8439418618021d6
TantoFaz <thiago.playgames15@gmail.com>
2023-06-11 11:12:59 -0300

    update: docs

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r") as file:
 
 setup(
     name="amazon-affiliate-scraper",
-    version="1.24.2",
+    version="0.1.1",
     license="MIT License",
     author="Marcuth",
     long_description=readme_content,
     long_description_content_type="text/markdown",
     author_email="marcuth2006@gmail.com",
     keywords="amazon affiliate scraper",
-    description=f"Utilities and tools for things related to Dragon City",
+    description="A simple library to help a developer who is affiliated with Amazon to automate the process.",
     packages=["amazon_affiliate"] + [ "amazon_affiliate/" + x for x in find_packages("amazon_affiliate") ]
 )
