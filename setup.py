59037cb4b84e364b50580417d828665abcb50e00
TantoFaz <thiago.playgames15@gmail.com>
2023-06-11 11:19:45 -0300

    d

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as file:
 
 setup(
     name="amazon-affiliate-scraper",
-    version="0.1.1",
+    version="0.1.2",
     license="MIT License",
     author="Marcuth",
     long_description=readme_content,
