a08cb8b51a0cab1068cdd84db71f04d0e5bbfdfb
TantoFaz <thiago.playgames15@gmail.com>
2024-02-16 12:33:24 -0300

    Atualizando dependências e refatorando o código

--- README.md
+++ README.md
@@ -5,10 +5,10 @@
 ### Creating Affiliate URL:
 
 ```py
-from amazon_affiliate import Client
+from amazon_affiliate import AmazonAffiliate
 
 def main():
-    client = Client(
+    amazon_affiliate = AmazonAffiliate(
         cookies_file_path = "path/to/amazon_cookies.json", # Path to Amazon site cookies
         origin_url = "https://www.amazon.com.br/", # Amazon root URL in your country
         webdriver_file_path = "path/to/webdriver.exe", # Path to browser webdriver
@@ -16,7 +16,7 @@ def main():
     )
 
     # Create an affiliate URL like: https://amzn.to/3qAp1rZ
-    affiliate_url = client.create_affiliate_url("https://www.amazon.com.br/Introdu%C3%A7%C3%A3o-Programa%C3%A7%C3%A3o-com-Python-Algoritmos/dp/8575227181/?_encoding=UTF8&pd_rd_w=cLgOh&content-id=amzn1.sym.142cf30d-cd8e-4f67-a147-740799e27bf0&pf_rd_p=142cf30d-cd8e-4f67-a147-740799e27bf0&pf_rd_r=Q5SHBAZH69HSPBRR2MGS&pd_rd_wg=98FtW&pd_rd_r=ffa375c6-01c9-4e9c-9f87-2aef974a89af&ref_=pd_gw_ci_mcx_mr_hp_atf_m") 
+    affiliate_url = amazon_affiliate.create_affiliate_url("https://www.amazon.com.br/Introdu%C3%A7%C3%A3o-Programa%C3%A7%C3%A3o-com-Python-Algoritmos/dp/8575227181/?_encoding=UTF8&pd_rd_w=cLgOh&content-id=amzn1.sym.142cf30d-cd8e-4f67-a147-740799e27bf0&pf_rd_p=142cf30d-cd8e-4f67-a147-740799e27bf0&pf_rd_r=Q5SHBAZH69HSPBRR2MGS&pd_rd_wg=98FtW&pd_rd_r=ffa375c6-01c9-4e9c-9f87-2aef974a89af&ref_=pd_gw_ci_mcx_mr_hp_atf_m") 
 
     print(affiliate_url)
 
--- amazon_affiliate/client.py
+++ amazon_affiliate/client.py
@@ -1,18 +1,23 @@
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from pydantic import validate_arguments
+from pydantic import validate_call
 from selenium import webdriver
 from typing import Optional
+import time
 import json
 
 DEFAULT_ORIGIN_URL = "https://amazon.com.br/"
 
-class Client:
+driver_manager = ChromeDriverManager()
+
+class AmazonAffiliate:
+    @validate_call
     def __init__(
         self,
         cookies_file_path: str,
         origin_url: str = DEFAULT_ORIGIN_URL,
-        webdriver_file_path: Optional[str] = None,
+        webdriver_file_path: Optional[str] = driver_manager.install(),
         headless = True
     ) -> None:
         with open(cookies_file_path, "r+") as file:
@@ -23,7 +28,8 @@ class Client:
         if headless:
             options.add_argument("--headless")
 
-        service = Service(webdriver_file_path)
+        service = Service(executable_path = webdriver_file_path)
+
         self._driver = webdriver.Chrome(
             options = options,
             service = service
@@ -36,12 +42,16 @@ class Client:
         for cookie in cookies:
             self._driver.add_cookie(cookie)
 
-    @validate_arguments
+    @validate_call
     def create_affiliate_url(self, original_url: str) -> str:
         self._driver.get(original_url)
 
+        self._driver.implicitly_wait(10)
+
         link_creator_element = self._driver.find_element(By.CSS_SELECTOR, "#amzn-ss-text-link a")
         link_creator_element.click()
+
+        time.sleep(3)
 
         created_link_element = self._driver.find_element(By.CSS_SELECTOR, "#amzn-ss-text-shortlink-textarea")
         created_link = created_link_element.get_attribute("value")
@@ -50,6 +60,3 @@ class Client:
 
     def close(self) -> None:
         self._driver.close()
-
-    def __del__(self) -> None:
-        self.close()
